594fc5e80c0f0b71732ce58ef67e386a4efa8b2d
Wenfei Xian <2316469036@qq.com>
2023-10-22 17:53:56 +0200

    Add files via upload

---
+++ 02.chloroplast/Orthologous_Groups_distribution.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+
+data <- read.table("Orthogroups.GeneCount.csv.gz", header=TRUE, skip=1, stringsAsFactors=FALSE)
+non_zero_counts <- apply(data[,-1], 1, function(row) sum(row > 0))
+
+thresholds <- 1:100
+counts <- sapply(thresholds, function(threshold) sum(non_zero_counts > threshold))
+
+plot_data <- data.frame(threshold = thresholds, count = counts)
+
+y_value_at_20 <- plot_data$count[plot_data$threshold == 20]
+
+p <- ggplot(plot_data, aes(x=threshold, y=count)) +
+  geom_point() +
+  geom_vline(aes(xintercept=20), linetype="dashed", color="red") +
+  geom_text(aes(x=20, y=y_value_at_20, label=y_value_at_20), vjust=-0.5, hjust=-0.2, color="blue") +  # This line adds the label
+  labs(title="Number of Rows by Presence of Orthologous Groups in Species",
+       x="Number of Species in which Each Orthologous Group is Present", 
+       y="Number of Orthologous Groups") +
+  theme_minimal()
+
+print(p)
