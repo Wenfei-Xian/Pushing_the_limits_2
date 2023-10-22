37b1be85b1cf3e1920b6d5b2f612b6c735a80db8
Wenfei Xian <2316469036@qq.com>
2023-10-22 18:30:13 +0200

    Update Orthologous_Groups_distribution.R

--- 02.chloroplast/Orthologous_Groups_distribution.R
+++ 02.chloroplast/Orthologous_Groups_distribution.R
@@ -1,22 +1,23 @@
 library(ggplot2)
 
-data <- read.table("Orthogroups.GeneCount.csv.gz", header=TRUE, skip=1, stringsAsFactors=FALSE)
+data <- read.table("Orthogroups.GeneCount.csv.gz", header=TRUE, stringsAsFactors=FALSE, sep="\t")
 non_zero_counts <- apply(data[,-1], 1, function(row) sum(row > 0))
 
 thresholds <- 1:100
-counts <- sapply(thresholds, function(threshold) sum(non_zero_counts > threshold))
+counts <- sapply(thresholds, function(threshold) sum(non_zero_counts >= threshold))
 
 plot_data <- data.frame(threshold = thresholds, count = counts)
 
-y_value_at_20 <- plot_data$count[plot_data$threshold == 20]
+y_value_at_20 <- plot_data$count[plot_data$threshold == 21]
 
 p <- ggplot(plot_data, aes(x=threshold, y=count)) +
   geom_point() +
-  geom_vline(aes(xintercept=20), linetype="dashed", color="red") +
-  geom_text(aes(x=20, y=y_value_at_20, label=y_value_at_20), vjust=-0.5, hjust=-0.2, color="blue") +  # This line adds the label
+  geom_vline(aes(xintercept=21), linetype="dashed", color="red") +
+  geom_text(aes(x=21, y=y_value_at_20, label=y_value_at_20), vjust=-0.5, hjust=-0.2, color="blue") +  # This line adds the label
   labs(title="Distribution of Orthologous Groups",
        x="Number of Species in which Each Orthologous Group is Present", 
        y="Number of Orthologous Groups") +
   theme_minimal()
 
 print(p)
+print(plot_data)
